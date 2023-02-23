d3f2868f0d4c1b0fdee83fea56a8af55cb363833
grazomarin <huseynovk355@gmail.com>
2023-02-23 20:39:44 +0400

    feat: add react to project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import Header from './components/Header';
+import Board from './components/Board';
+
+const App = () => {
+	return (
+		<>
+		</>
+	);
+};
+
+export default App;
